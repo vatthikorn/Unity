251beb0461808a65c916b50a80b6c20692b2dcce
Nathan Cruz <nathan.cruz.q@gmail.com>
2016-03-23 17:26:55 -0400

    Added files via upload

---
+++ DoorWay.cs
@@ -0,0 +1,27 @@
+﻿/*
+    Signals to the Room class that the player has collided with it.
+    This is attached to an entrance to the room.
+    There should be multiple in most rooms.
+
+    Required:
+    Attached to cube gameObject (w/o material)
+    gameObject has isTrigger set
+    gameObject has a parent (The Room)
+    The parent has a "Room" Script component
+    The player gameObject has the "Player" tag
+
+*/
+using UnityEngine;
+using System.Collections;
+
+public class DoorWay : MonoBehaviour {
+
+    //Signals to the room it has been entered, calls its entered()
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            transform.parent.gameObject.GetComponent<Room>().entered();
+        }
+    }
+}
---
+++ Room.cs
@@ -0,0 +1,62 @@
+﻿/*
+    Keeps track of the status of the room.
+    This is attached to a single room.
+
+    Required:
+    Attached to a room GameObject
+    The room GameObject has doorways
+    The room GameObject has a parent (The Zone)
+
+    Remember to:
+    Set up the roomType beforehand
+    Set up the playerIsIn and found to true for rooms the player starts in
+    Set up room status from Unity
+    Place one at the entrance of every doorway
+*/
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Room : MonoBehaviour {
+
+    //undiscovered ... playerinside - self-explanatory
+    //boss - the room where the boss is
+    //bossBattle - when the player enters the room and the boss is not dead yet
+    //defeatedInside - when the boss is dead and player is inside the room
+    //defeatedOutside - when the boss is dead and player is in another room
+    public enum RoomState { undiscovered, hidden, discovered, playerInside, boss, bossBattle, defeatedInside, defeatedOutside};
+    
+    //Initial State: undiscovered, hidden or boss (or playerInside for room the player appears in)
+    public RoomState roomType;
+
+    //undiscovered || hidden || discovered -> playerInside
+    //defeatedOutside -> defeatedInside
+    //boss -> bossBattle
+    //Is called by a doorway when player collides with it
+    public void entered()
+    {
+        transform.parent.GetComponent<RoomTracker>().resetAll();
+
+        if (roomType == RoomState.boss)
+            roomType = RoomState.bossBattle;
+        else if (roomType == RoomState.defeatedOutside)
+            roomType = RoomState.defeatedInside;
+        else
+            roomType = RoomState.playerInside;
+
+        transform.parent.GetComponent<RoomTracker>().UpdateMap();
+    }
+
+    //playerInside -> discovered
+    //defeatedInside -> defeatedOustide
+    //hidden, undiscovered and boss stay the same
+    //bossbattle should never be a situation
+    //called by the Zone to reset all rooms to proper state before updating the map
+    public void exited()
+    {
+        if (roomType == RoomState.defeatedInside)
+            roomType = RoomState.defeatedOutside;
+        else if (roomType == RoomState.playerInside)
+            roomType = RoomState.discovered;
+    }
+}
