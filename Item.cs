083bd11896c779fee1cfc52175d6763f55890c25
Nathan Cruz <nathan.cruz.q@gmail.com>
2016-03-24 14:18:50 -0400

    Added files via upload

---
+++ Item.cs
@@ -0,0 +1,34 @@
+﻿/*
+    This is to keep information on an item in the game for the purposes of inventory and equipment.
+    This is NOT to for items dropped in game or released from chests. (This is for a separate system)
+
+    Required:
+    Set values to 0 when appropriate.
+*/
+using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Item {
+
+    public enum ItemType { weapon, shield, armor, sigil, consumable, keyItem };
+
+    public int itemID;
+    public string itemName;
+    public string itemDesc;
+    public ItemType itemType;
+    public Texture2D icon;
+
+    //Weapons only, otherwise set to 0
+    public int strength;
+    public float criticalChance;
+
+    //Shields only, otherwise set to 0
+    public float damageMitigation;
+
+    //Armor only, otherwise set to 0
+    public int defense;
+
+    //Sigil only
+    public GameObject sigil;//the gameObject will be attached with a Sigil script that contains all information and behavior.
+}
---
+++ ItemDatabase.cs
@@ -0,0 +1,14 @@
+﻿/*
+    This is to keep of every item in the game for the purposes of inventory and equipment.
+
+    Required:
+    Attached to an empty object in screen.
+*/
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ItemDatabase : MonoBehaviour {
+
+    public List<Item> items = new List<Item>();
+}
