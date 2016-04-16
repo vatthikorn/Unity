e049a447263afe51ebe343ffeae6fcf69035da9d
Zack Apiratitham <zackvatthikorn@gmail.com>
2016-04-16 17:55:39 -0400

    main menu

---
+++ Sigilscape/Assets/Scripts/Controls/MainMenuControl.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MainMenuControl : MonoBehaviour {
+
+	public GameObject canvas;
+
+	//private GameObject howToPlay;
+
+	void Start() {
+	}
+
+	// Use this for initialization
+	public void ShowImage() {
+
+		//howToPlay = this.canvas.GetComponent<GameObject> (); 
+
+		Debug.Log ("Show Image here");
+
+		canvas.SetActive (true);
+
+	}
+
+
+	public void HideImage() {
+
+		Debug.Log ("Hide Image");
+
+		canvas.SetActive (false);
+
+	}
+
+}
