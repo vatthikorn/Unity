9ebfb551ebe4e191d2162fb5d5c75dcf9d05e9d9
unknown <nathan.cruz.q@gmail.com>
2016-04-21 11:06:48 -0400

    Decorations

---
+++ Sigilscape/Assets/Scripts/SecretRoomReveal.cs
@@ -0,0 +1,20 @@
+﻿/*
+    Attach to hidden room object.
+    Keeps the room unrevealed when the player
+*/
+using UnityEngine;
+using System.Collections;
+
+public class SecretRoomReveal : MonoBehaviour {
+
+    public GameObject wall;
+    
+    void Update()
+    {
+        if(this.gameObject.GetComponent<Room>().roomType != Room.RoomState.hidden)
+        {
+            wall.SetActive(false);
+        }
+    }	
+	
+}
